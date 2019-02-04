4e81ec0639344b7bca3be3052ec752eedeaa2b6e
Sarah Brood <sarahbrood@gmail.com>
2019-02-04 11:28:17 +0100

    puissance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import {PokemonComponent} from './pokemon/pokemon.component';
 import {TypeComponent} from './type/type.component';
 import {MovesComponent} from './moves/moves.component';
 import {RouterModule, Routes} from '@angular/router';
+import {CalculComponent} from './calcul/calcul.component';
 
 
 const routes: Routes = [
   {path: 'types', component: TypeComponent },
   {path : '', redirectTo: '/types', pathMatch: 'full'},
   {path : 'pokemon', component : PokemonComponent},
-  {path: 'moves', component: MovesComponent}
+  {path: 'moves/:id', component: MovesComponent},
+  {path : 'calcul/:nomPokeA/:attackName/:lvlA/:nomPokeB/:lvlB', component : CalculComponent}
 ];
 
 @NgModule({
